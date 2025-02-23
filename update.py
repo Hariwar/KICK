d20a7ad8c1949b73eebcc0b9d81adc987c2a93a6
Hariwar <105588706+Hariwar@users.noreply.github.com>
2025-02-23 14:44:26 +0530

    Update update.py

--- update.py
+++ update.py
@@ -1,19 +1,29 @@
-from sys import exit
-from dotenv import load_dotenv, dotenv_values
+import os
+from datetime import datetime
+from importlib import import_module
 from logging import (
-    FileHandler,
-    StreamHandler,
-    INFO,
-    basicConfig,
-    error as log_error,
-    info as log_info,
-    getLogger,
     ERROR,
+    INFO,
+    FileHandler,
+    Formatter,
+    LogRecord,
+    StreamHandler,
+    basicConfig,
+    getLogger,
 )
-from os import path, environ, remove
+from logging import (
+    error as log_error,
+)
+from logging import (
+    info as log_info,
+)
+from os import path, remove
+from subprocess import run as srun
+from sys import exit
+
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-from subprocess import run as srun
+from pytz import timezone
 
 getLogger("pymongo").setLevel(ERROR)
 
@@ -24,62 +34,93 @@ if path.exists("log.txt"):
 if path.exists("rlog.txt"):
     remove("rlog.txt")
 
-basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    handlers=[FileHandler("log.txt"), StreamHandler()],
-    level=INFO,
+
+class CustomFormatter(Formatter):
+    def formatTime(  # noqa: N802
+        self,
+        record: LogRecord,
+        datefmt: str | None,
+    ) -> str:
+        dt: datetime = datetime.fromtimestamp(
+            record.created,
+            tz=timezone("Asia/Dhaka"),
+        )
+        return dt.strftime(datefmt)
+
+    def format(self, record: LogRecord) -> str:
+        return super().format(record).replace(record.levelname, record.levelname[:1])
+
+
+formatter = CustomFormatter(
+    "[%(asctime)s] %(levelname)s - %(message)s [%(module)s:%(lineno)d]",
+    datefmt="%d-%b %I:%M:%S %p",
 )
 
-load_dotenv("config.env", override=True)
+file_handler = FileHandler("log.txt")
+file_handler.setFormatter(formatter)
 
+stream_handler = StreamHandler()
+stream_handler.setFormatter(formatter)
+
+basicConfig(handlers=[file_handler, stream_handler], level=INFO)
+
+# Attempt to load from config.py
 try:
-    if bool(environ.get("_____REMOVE_THIS_LINE_____")):
-        log_error("The README.md file there to be read! Exiting now!")
-        exit(1)
-except:
-    pass
+    settings = import_module("config")
+    config_file = {
+        key: value.strip() if isinstance(value, str) else value
+        for key, value in vars(settings).items()
+    }
+except Exception:
+    log_error(
+        "The 'config.py' file is missing! Falling back to environment variables.",
+    )
+    config_file = {}
 
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-if len(BOT_TOKEN) == 0:
-    log_error("BOT_TOKEN variable is missing! Exiting now")
+# Fallback to environment variables if BOT_TOKEN is not set
+BOT_TOKEN = config_file.get("BOT_TOKEN") or os.getenv("BOT_TOKEN")
+if not BOT_TOKEN:
+    log_error("BOT_TOKEN variable is missing! Exiting now.")
     exit(1)
 
 BOT_ID = BOT_TOKEN.split(":", 1)[0]
 
-DATABASE_URL = environ.get("DATABASE_URL", "")
-if len(DATABASE_URL) == 0:
-    DATABASE_URL = None
+# Fallback to environment variables for DATABASE_URL
+DATABASE_URL = config_file.get("DATABASE_URL", "") or os.getenv("DATABASE_URL", "")
 
-if DATABASE_URL is not None:
+if DATABASE_URL:
     try:
         conn = MongoClient(DATABASE_URL, server_api=ServerApi("1"))
-        db = conn.mltb
-        old_config = db.settings.deployConfig.find_one({"_id": BOT_ID})
+        db = conn.luna
         config_dict = db.settings.config.find_one({"_id": BOT_ID})
-        if old_config is not None:
-            del old_config["_id"]
-        if (
-            old_config is not None
-            and old_config == dict(dotenv_values("config.env"))
-            or old_config is None
-        ) and config_dict is not None:
-            environ["UPSTREAM_REPO"] = config_dict["UPSTREAM_REPO"]
-            environ["UPSTREAM_BRANCH"] = config_dict["UPSTREAM_BRANCH"]
+        if config_dict is not None:
+            config_file["UPSTREAM_REPO"] = config_dict.get(
+                "UPSTREAM_REPO",
+                config_file.get("UPSTREAM_REPO"),
+            )
+            config_file["UPSTREAM_BRANCH"] = config_dict.get(
+                "UPSTREAM_BRANCH",
+                config_file.get("UPSTREAM_BRANCH"),
+            )
         conn.close()
     except Exception as e:
         log_error(f"Database ERROR: {e}")
 
-UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "")
-if len(UPSTREAM_REPO) == 0:
-    UPSTREAM_REPO = None
+UPSTREAM_REPO = (
+    config_file.get("UPSTREAM_REPO", "")
+    or os.getenv("UPSTREAM_REPO", "")
+    or "https://github.com/AeonOrg/Aeon-MLTB"
+)
 
-UPSTREAM_BRANCH = environ.get("UPSTREAM_BRANCH", "")
-if len(UPSTREAM_BRANCH) == 0:
-    UPSTREAM_BRANCH = "master"
+UPSTREAM_BRANCH = (
+    config_file.get("UPSTREAM_BRANCH", "")
+    or os.getenv("UPSTREAM_BRANCH", "")
+    or "main"
+)
 
-if UPSTREAM_REPO is not None:
+if UPSTREAM_REPO:
     if path.exists(".git"):
-        srun(["rm", "-rf", ".git"])
+        srun(["rm", "-rf", ".git"], check=False)
 
     update = srun(
         [
@@ -90,14 +131,15 @@ if UPSTREAM_REPO is not None:
                      && git commit -sm update -q \
                      && git remote add origin {UPSTREAM_REPO} \
                      && git fetch origin -q \
-                     && git reset --hard origin/{UPSTREAM_BRANCH} -q"
+                     && git reset --hard origin/{UPSTREAM_BRANCH} -q",
         ],
         shell=True,
+        check=False,
     )
 
     if update.returncode == 0:
         log_info("Successfully updated with latest commit from UPSTREAM_REPO")
     else:
         log_error(
-            "Something went wrong while updating, check UPSTREAM_REPO if valid or not!"
+            "Something went wrong while updating, check UPSTREAM_REPO if valid or not!",
         )
