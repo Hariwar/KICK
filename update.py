90e485b8e446af24192341e78a0fe6310a9fcf06
5hojib <yesiamshojib@gmail.com>
2025-02-23 09:14:38 +0000

    Auto-format code [skip actions]

--- update.py
+++ update.py
@@ -113,9 +113,7 @@ UPSTREAM_REPO = (
 )
 
 UPSTREAM_BRANCH = (
-    config_file.get("UPSTREAM_BRANCH", "")
-    or os.getenv("UPSTREAM_BRANCH", "")
-    or "main"
+    config_file.get("UPSTREAM_BRANCH", "") or os.getenv("UPSTREAM_BRANCH", "") or "main"
 )
 
 if UPSTREAM_REPO:
